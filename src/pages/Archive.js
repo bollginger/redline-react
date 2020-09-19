599d09c0aca83b95de9f3c6e0f01e88ea5e50b0a
bollginger <62157795+bollginger@users.noreply.github.com>
2020-09-19 01:26:57 -0400

    Zine preview covers changed to jpg
    - Made active nav tab underline thinner

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { redlineIssue1, redlineIssue2 } from '../zines/zineVariables';
-import ZinePreview from '../components/ZinePreview';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  redlineIssue1,
+  redlineIssue2,
+  redlineIssue3,
+} from "../zines/zineVariables";
+import ZinePreview from "../components/ZinePreview";
 
-const Archive = ({pdfWidth}) => {
-
+const Archive = ({ pdfWidth }) => {
   // Add additional issues to this list
-  let issues = [redlineIssue1, redlineIssue2];
+  let issues = [redlineIssue1, redlineIssue2, redlineIssue3];
 
-  let issuesDisplay = issues.map(issueInfo => {
-    let pageUrl = '/archive-issue' + issueInfo.issueNumber;
+  let issuesDisplay = issues.map((issueInfo) => {
+    let pageUrl = "/archive-issue" + issueInfo.issueNumber;
     return (
       <>
         <div>
           <NavLink to={pageUrl}>
-            <ZinePreview pdf={issueInfo.pdf} pdfWidth={pdfWidth}/>
+            <ZinePreview page={issueInfo.firstPage} pdfWidth={pdfWidth} />
           </NavLink>
-          <a href={issueInfo.path} download>Ed.1, Vol:{issueInfo.issueNumber}, {issueInfo.issueDate}</a>
+          <a href={issueInfo.path} download>
+            No. {issueInfo.issueNumber}, {issueInfo.issueDate}
+          </a>
         </div>
-        <div class='archiveIssuePadding'/>
+        <div class="archiveIssuePadding" />
       </>
-    )
+    );
   });
 
-  return (
-    <div className='archive'>
-      {issuesDisplay}
-    </div>
-)};
-
-
-
+  return <div className="archive">{issuesDisplay}</div>;
+};
 
 export default Archive;
--- src/zines/zineVariables.js
+++ src/zines/zineVariables.js
@@ -1,20 +1,33 @@
 // Add future zine issues here
-import redline2PDF from "./redline_issue_2_2020.pdf";
-import redline1PDF from "./redline_issue_1_2020.pdf";
-
 
 export const redlineIssue1 = {
-    issueNumber: 1,
-    issueDate: 'June 2020',
-    pdf: redline1PDF,
-    path: "../zines/redline_issue_1_2020.pdf",
-    download: "./zines/redline_issue_1_2020.pdf"
+  issueNumber: 1,
+  issueDate: "June 2020",
+  firstPage: "../zineFirstPages/issue2.master1.jpg",
+  path: "../zines/redline_issue_1_2020.pdf",
+  download: "./zines/redline_issue_1_2020.pdf",
 };
-  
+
 export const redlineIssue2 = {
-    issueNumber: 2,
-    issueDate: 'July 2020',
-    pdf: redline2PDF,
-    path: "../zines/redline_issue_2_2020.pdf",
-    download: "./zines/redline_issue_2_2020.pdf"
+  issueNumber: 2,
+  issueDate: "July 2020",
+  firstPage: "../zineFirstPages/issue2.master1.jpg",
+  path: "../zines/redline_issue_2_2020.pdf",
+  download: "./zines/redline_issue_2_2020.pdf",
+};
+
+export const redlineIssue3 = {
+  issueNumber: 3,
+  issueDate: "July 2020",
+  firstPage: "../zineFirstPages/issue3.master1.jpg",
+  path: "../zines/redline_issue_2_2020.pdf",
+  download: "./zines/redline_issue_2_2020.pdf",
+};
+
+export const redlineIssue4 = {
+  issueNumber: 4,
+  issueDate: "August 2020",
+  firstPage: "../zineFirstPages/issue4.master1.jpg",
+  path: "../zines/redline_issue_2_2020.pdf",
+  download: "./zines/redline_issue_2_2020.pdf",
 };
